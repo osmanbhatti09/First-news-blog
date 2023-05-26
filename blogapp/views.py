272317280dc0e22d91cc6cfe76993ba4db2c0b1d
osmanbhatti09 <usmanbhatti.bu@gmail.com>
2023-05-27 02:41:07 +0500

    all the html files to git

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'post_list.html', {})
